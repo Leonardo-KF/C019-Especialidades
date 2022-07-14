15e8d23038cf7214c989bd0ffb3176191079eed0
Leonardo Keiser Fleck <89053412+Leonardo-KF@users.noreply.github.com>
2022-07-14 20:16:43 -0300

    implementation of authentication in client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./index.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { user, loginWithPopup, getAccessTokenSilently } = useAuth0();
+
+  async function getToken() {
+    const token = await getAccessTokenSilently({ scope: "read:current_user" });
+    console.log(token);
+    console.log(user);
+  }
+
+  useEffect(() => {
+    getToken();
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello world</h1>
+      <button onClick={() => loginWithPopup()}>Login</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-jtuw45s1.us.auth0.com"
+      clientId="ZS6Xk6rRb2nRaLBiFWxGO0uJuHXFM79z"
+      redirectUri={window.location.origin}
+      audience="auth-service-financial-app"
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
-)
+);
